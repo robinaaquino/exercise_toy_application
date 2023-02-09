45b0cfb3c62d7d731a07b57b4588e145745e149d
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-10 00:08:46 +0800

    feat: modified migration file

--- db/migrate/20230209155903_table_users.rb
+++ db/migrate/20230209155903_table_users.rb
@@ -1,4 +1,7 @@
 class TableUsers < ActiveRecord::Migration[7.0]
   def change
+    create_table :users do |user|
+      user.string :name
+      user.string :email
   end
 end
--- db/migrate/20230209160005_table_records.rb
+++ db/migrate/20230209160005_table_records.rb
@@ -1,4 +1,7 @@
 class TableRecords < ActiveRecord::Migration[7.0]
   def change
+    create_table :records do |record|
+      record.text :content
+      record.integer :record_id
   end
 end
