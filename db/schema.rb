e61ad7cb768f294ac3cf01e3ba27ae35a0ff3fbd
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-15 00:05:29 +0800

    feat: fixed local environment for server

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_09_160005) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "records", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
