779d0c33a197080230a78b6d67eeb79faf67bd38
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-16 21:38:05 +0800

    feat: created a migration file to add password to users and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
-    has_many :record
-    validates :name, presence: true
-    validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  before_save { email.downcase! }
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true,
+      length: { maximum: 255 },
+      format: { with: VALID_EMAIL_REGEX },
+      uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
