306b43333d2535bface83ce4eb0ed0593e0041f7
Robina Rhamz Aquino <robinarhamzaquino@gmail.com>
2023-02-16 21:50:53 +0800

    feat: modified user_parameters to include password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,52 +1,52 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[ show edit update destroy ]
 
-    def index
-        @users = User.all
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_parameters)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to users_url, notice: "Successfully created a user"}
+      end
     end
+  end
 
-    def show
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_parameters)
+        format.html { redirect_to user_url(@user), notice: "Successfully updated a user"}
+      end
     end
+  end
 
-    def new
-        @user = User.new
+  def destroy
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: "Successfully deleted a user"}
     end
+  end
 
-    def create
-        @user = User.new(user_parameters)
+  private
+      def set_user
+        @user = User.find(params[:id])
+      end
 
-        respond_to do |format|
-            if @user.save
-                format.html { redirect_to users_url, notice: "Successfully created a user"}
-            end
-        end
-    end
-
-    def edit
-    end
-
-    def update
-        respond_to do |format|
-            if @user.update(user_parameters)
-                format.html { redirect_to user_url(@user), notice: "Successfully updated a user"}
-            end
-        end
-    end
-
-    def destroy
-        @user.destroy
-
-        respond_to do |format|
-            format.html { redirect_to users_url, notice: "Successfully deleted a user"}
-        end
-    end
-
-    private
-        def set_user
-            @user = User.find(params[:id])
-        end
-
-        def user_parameters
-            params.require(:user).permit(:name, :email)
-        end
+      def user_parameters
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 end
